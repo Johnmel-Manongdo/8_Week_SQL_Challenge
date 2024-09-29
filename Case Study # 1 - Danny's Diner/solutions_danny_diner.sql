0521c14622d1707f512194a4948d493cef4dc750
Johnmel <38127581+Johnmel-Manongdo@users.noreply.github.com>
2024-09-30 00:15:26 +0800

    initial commit

--- Case Study # 1 - Danny's Diner/solutions_danny_diner.sql
+++ Case Study # 1 - Danny's Diner/solutions_danny_diner.sql
@@ -18,7 +18,7 @@ how many points do customer A and B have at the end of January?
 -- 1. What is the total amount each customer spent at the restaurant?
 
 SELECT 
-	sales.customer_id, 
+    sales.customer_id, 
     SUM(menu.price) AS total_spent
 FROM sales
 INNER JOIN menu ON sales.product_id = menu.product_id
@@ -27,7 +27,7 @@ GROUP BY sales.customer_id;
 -- 2. How many days has each customer visited the restaurant?
 
 SELECT 
-	customer_id, 
+    customer_id, 
     COUNT(DISTINCT(order_date)) AS total_days_spent
 FROM sales
 GROUP BY customer_id;
@@ -35,17 +35,17 @@ GROUP BY customer_id;
 -- 3. What was the first item from the menu purchased by each customer?
 
 WITH ordered_rank AS (
-	SELECT 
-		sales.customer_id, 
+    SELECT 
+        sales.customer_id, 
         sales.order_date,
-		menu.product_name,
-		DENSE_RANK() OVER(PARTITION BY sales.customer_id ORDER BY sales.order_date) AS date_rank
-	FROM sales
+	menu.product_name,
+	DENSE_RANK() OVER(PARTITION BY sales.customer_id ORDER BY sales.order_date) AS date_rank
+    FROM sales
     INNER JOIN menu ON sales.product_id = menu.product_id
 )
 
 SELECT 
-	customer_id, 
+    customer_id, 
     order_date, 
     product_name
 FROM ordered_rank
@@ -55,11 +55,10 @@ GROUP BY customer_id, order_date, product_name;
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 
 SELECT 
-  menu.product_name,
-  COUNT(sales.product_id) AS total_order
+    menu.product_name,
+    COUNT(sales.product_id) AS total_order
 FROM sales
-INNER JOIN menu
-  ON sales.product_id = menu.product_id
+INNER JOIN menu ON sales.product_id = menu.product_id
 GROUP BY menu.product_name
 ORDER BY total_order DESC
 LIMIT 1;
@@ -67,17 +66,18 @@ LIMIT 1;
 -- 5. Which item was the most popular for each customer?
 
 WITH order_info AS (
-  SELECT 
-    sales.customer_id, 
-    menu.product_name,
-    COUNT(sales.product_id) AS total_count,
-    DENSE_RANK() OVER(PARTITION BY sales.customer_id ORDER BY COUNT(sales.customer_id) DESC) AS ranking
+    SELECT 
+        sales.customer_id, 
+        menu.product_name,
+        COUNT(sales.product_id) AS total_count,
+        DENSE_RANK() OVER(PARTITION BY sales.customer_id ORDER BY COUNT(sales.customer_id) DESC) AS ranking
   FROM sales
   INNER JOIN menu ON sales.product_id = menu.product_id
   GROUP BY sales.customer_id, menu.product_name
 )
+	
 SELECT 
-	customer_id, 
+    customer_id, 
     total_count, 
     product_name
 FROM order_info
@@ -86,19 +86,23 @@ WHERE ranking = 1;
 -- 6. Which item was purchased first by the customer after they became a member?
 
 WITH after_join_date AS (
-	SELECT 
-		sales.customer_id, 
+    SELECT 
+        sales.customer_id, 
         menu.product_name,
         sales.order_date, 
         members.join_date,
         RANK() OVER(PARTITION BY sales.customer_id ORDER BY sales.order_date) AS ranking
-	FROM sales
-	INNER JOIN members ON sales.customer_id = members.customer_id
+    FROM sales
+    INNER JOIN members ON sales.customer_id = members.customer_id
     INNER JOIN menu ON sales.product_id = menu.product_id
-	WHERE members.join_date <= sales.order_date
+    WHERE members.join_date <= sales.order_date
 )
 
-SELECT customer_id, product_name, order_date, join_date
+SELECT 
+    customer_id, 
+    product_name, 
+    order_date, 
+    join_date
 FROM after_join_date
 WHERE ranking = 1
 ORDER BY customer_id;
@@ -106,19 +110,23 @@ ORDER BY customer_id;
 -- 7. Which item was purchased just before the customer became a member?
 
 WITH before_join_date AS (
-	SELECT 
-		sales.customer_id, 
+    SELECT 
+        sales.customer_id, 
         menu.product_name,
         sales.order_date, 
         members.join_date,
         RANK() OVER(PARTITION BY sales.customer_id ORDER BY sales.order_date DESC) AS ranking
-	FROM sales
-	INNER JOIN members ON sales.customer_id = members.customer_id
+    FROM sales
+    INNER JOIN members ON sales.customer_id = members.customer_id
     INNER JOIN menu ON sales.product_id = menu.product_id
-	WHERE members.join_date > sales.order_date
+    WHERE members.join_date > sales.order_date
 )
 
-SELECT customer_id, product_name, order_date, join_date
+SELECT 
+    customer_id, 
+    product_name, 
+    order_date, 
+    join_date
 FROM before_join_date
 WHERE ranking = 1
 ORDER BY customer_id;
@@ -126,7 +134,7 @@ ORDER BY customer_id;
 -- 8. What is the total items and amount spent for each member before they became a member?
 
 SELECT 
-	sales.customer_id,
+    sales.customer_id,
     COUNT(sales.product_id) AS total_items,
     SUM(menu.price) AS total_amount
 FROM sales
@@ -139,19 +147,19 @@ ORDER BY sales.customer_id;
 -- 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
 
 WITH cte_points AS (
-SELECT
-	sales.customer_id,
-    menu.price,
-    CASE
-		WHEN menu.product_name = "sushi" THEN (menu.price * 10) * 2
-        ELSE menu.price * 10
+    SELECT
+        sales.customer_id,
+        menu.price,
+        CASE
+            WHEN menu.product_name = "sushi" THEN (menu.price * 10) * 2
+            ELSE menu.price * 10
 	END AS points
 FROM sales
 INNER JOIN menu ON sales.product_id = menu.product_id
 )
 
 SELECT 
-	customer_id,
+    customer_id,
     SUM(price) AS total_spent,
     SUM(points) AS total_points
 FROM cte_points
@@ -161,19 +169,19 @@ GROUP BY customer_id;
 -- how many points do customer A and B have at the end of January?		
 
 WITH cte_date AS (
-SELECT
-	join_date,
-    DATE_ADD(join_date, INTERVAL 6 DAY) AS one_week,
-    customer_id,
-    LAST_DAY(join_date) AS end_of_month
-FROM members
+    SELECT
+        join_date,
+        DATE_ADD(join_date, INTERVAL 6 DAY) AS one_week,
+        customer_id,
+        LAST_DAY(join_date) AS end_of_month
+    FROM members
 )
 
 SELECT 
-	sales.customer_id,
+    sales.customer_id,
     SUM(CASE 
-		WHEN order_date BETWEEN join_date AND one_week THEN price * 10 * 2
-        ELSE price * 10
+            WHEN order_date BETWEEN join_date AND one_week THEN price * 10 * 2
+            ELSE price * 10
 	END) AS points
 FROM menu
 INNER JOIN sales ON menu.product_id = sales.product_id
@@ -187,44 +195,40 @@ ORDER BY sales.customer_id;
 -- Join All The Things
 
 SELECT 
-  sales.customer_id, 
-  sales.order_date,  
-  menu.product_name, 
-  menu.price,
-  CASE
-    WHEN sales.order_date >= members.join_date THEN 'Y'
-    ELSE 'N' END AS member
-FROM sales
-INNER JOIN menu
-  ON sales.product_id = menu.product_id
-LEFT JOIN members
-  ON sales.customer_id = members.customer_id
-ORDER BY sales.customer_id, sales.order_date;
-
--- Rank All The Things
-
-WITH cte_ranking AS (
-SELECT 
     sales.customer_id, 
     sales.order_date,  
     menu.product_name, 
     menu.price,
     CASE
         WHEN sales.order_date >= members.join_date THEN 'Y'
-        ELSE 'N' 
-    END AS member
+        ELSE 'N' END AS member
 FROM sales
-INNER JOIN menu
-    ON sales.product_id = menu.product_id
-LEFT JOIN members
-    ON sales.customer_id = members.customer_id
+INNER JOIN menu ON sales.product_id = menu.product_id
+LEFT JOIN members ON sales.customer_id = members.customer_id
+ORDER BY sales.customer_id, sales.order_date;
+
+-- Rank All The Things
+
+WITH cte_ranking AS (
+    SELECT 
+        sales.customer_id, 
+        sales.order_date,  
+        menu.product_name, 
+        menu.price,
+        CASE
+            WHEN sales.order_date >= members.join_date THEN 'Y'
+            ELSE 'N' 
+        END AS member
+FROM sales
+INNER JOIN menu ON sales.product_id = menu.product_id
+LEFT JOIN members ON sales.customer_id = members.customer_id
 ORDER BY sales.customer_id, sales.order_date
 )
 
 SELECT 
-	*,
-	CASE
-		WHEN member = 'N' THEN NULL
+    *,
+    CASE
+        WHEN member = 'N' THEN NULL
         ELSE DENSE_RANK() OVER (PARTITION BY customer_id, member ORDER BY order_date)
     END AS ranking
 FROM cte_ranking
